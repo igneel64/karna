20650a35fd60e0a0520ef5816c528f9c2d79dcb3
Petros <p.perlepes@gmail.com>
2019-11-27 00:14:17 +0200

    Adding external library inclusion :package:

--- lib/types/types.d.ts
+++ lib/types/types.d.ts
@@ -22,5 +22,16 @@ export interface Resolver{
    * @param absPath Absolute dependency path
    * @returns Resolved path with extension
    */
-  resolve(absPath: string): string; 
+  resolve(absPath: string): string;
+  /**
+   * @description Resolves an extensionless import based on the Configuration allowed extensions
+   * @param extensionlessPath Path for an extensionless import
+   * @returns Resolved path with extension
+   */
+  resolveExtensionInOrder(extensionlessPath: string): string;
+  /**
+   * @param moduleName Name of the resource module to include
+   * @returns Resolved root path for new module 
+   */
+  resolveRootModule(moduleName: string): string;
 }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 var s = require("./message.js");
+var f = require("lodash.sortby");
 let a = 1;
 console.log(s + a);
