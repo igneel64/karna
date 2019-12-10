7f9440559916441635aa3ad40e9fc1d36b3dd522
Petros <p.perlepes@gmail.com>
2019-12-10 14:13:32 +0200

    Async writting

--- test/lib/run.spec.js
+++ test/lib/run.spec.js
@@ -8,14 +8,15 @@ const FIXTURES_DIR = path.resolve(__dirname, "../fixtures");
 
 describe("sane bundles", () => {
   const dirs = fs.readdirSync(FIXTURES_DIR);
-    it.each(dirs)("building %s", projectDir => {
+    it.each(dirs)("building %s", async projectDir => {
       const $compilationProxy = new Proxy(new Compilation(), CompilationTimingHandler);
       let resolvedDir = path.resolve(FIXTURES_DIR, projectDir);
       const prevDir = process.cwd();
       process.chdir(resolvedDir);
 
-      karna({ output: "./dist", entry: "./src/index.js"}, $compilationProxy);
+      const result = await karna({ output: "./dist", entry: "./src/index.js"}, $compilationProxy);
 
       process.chdir(prevDir);
+      return result;
     });
 });
